208da23853039b179c47ae68da3fae045817cedd
gaperez64 <gaperez64@gmail.com>
2025-06-09 15:47:30 +0200

    we are ready for graphs in terms of hindex

--- comp.py
+++ comp.py
@@ -34,9 +34,9 @@ bdsets = {"QEST+FORMATS": "qest+formats-scopus.csv",
           "ATVA": "atva-scopus.csv",
           "FMCAD": "fmcad-scopus.csv",
           "VMCAI": "vmcai-scopus.csv",
-          "AiML": "aiml-scopus.csv",
+          # "AiML": "aiml-scopus.csv",
           "ARES": "ares-scopus.csv",
-          "FCT": "fct-scopus.csv",
+          # "FCT": "fct-scopus.csv",
           "FSCD": "fscd-scopus.csv",
           "ICPE": "icpe-scopus.csv",
           "IFM": "ifm-scopus.csv",
@@ -49,19 +49,40 @@ bdsets = {"QEST+FORMATS": "qest+formats-scopus.csv",
           "SAGT": "sagt-scopus.csv",
           "SEFM": "sefm-scopus.csv",
           "SRDS": "srds-scopus.csv",
-          "WADS": "wads-scopus.csv",
+          # "WADS": "wads-scopus.csv",
           "WALCOM": "walcom-scopus.csv"}
 qpcts = [.05, .10, .25, .50]
-width = 0.8 / 20  # len(dsets)
+width = 0.8 / 18  # len(dsets)
 
 
-def load_bibs(fpath, dsname, ithbar):
+hdf = pd.read_csv("h_index.csv")
+
+
+def get_max_hindex(row):
+    if pd.isna(row["Author(s) ID"]):
+        return 0
+    # print(row)
+    ids = [int(a) for a in str(row["Author(s) ID"]).split(";")]
+    # print(ids)
+    # print(hdf.columns)
+    # print(hdf["id"].head())
+    mx = hdf.loc[hdf["id"].isin(ids)].max()
+    mx = mx["hidx"].astype(float)
+    return mx
+
+
+def load_bibs(fpath, dsname, ithbar, hidx=False):
     df = pd.read_csv(fpath)
     df = df.loc[df["Year"].isin(years)]
     df = df.loc[df["Document Type"] == "Conference paper"]
     npaps = len(df.index)
     print(f"No. of papers = {npaps}")
-    qs = df["Cited by"]
+    kcol = "Cited by"
+    if hidx:
+        kcol = "hindex"
+        df[kcol] = df.apply(get_max_hindex, axis=1)
+
+    qs = df[kcol]
 
     # Get the top quantiles instead of low
     qs = qs.quantile([1 - q for q in qpcts]).values.tolist()
@@ -69,7 +90,7 @@ def load_bibs(fpath, dsname, ithbar):
     counts = []
     for low in qs:
         print(f"low = {low}")
-        band = df.loc[(df["Cited by"] >= low)]
+        band = df.loc[(df[kcol] >= low)]
         counts.append(len(band.index))
     print(f"Papers per quant = {counts}")
     pcts = np.array(counts)  # [(100 * c / npaps) for c in counts])
@@ -81,7 +102,7 @@ def load_bibs(fpath, dsname, ithbar):
     # Return the aggreagated information: mean and median per year
     # df = df.groupby(df["Year"]).agg({"Cited by": ["mean", "median"]})
     # df.columns = [dsname + "_" + col[1] for col in df.columns]
-    return df["Cited by"].to_list()
+    return df[kcol].to_list()
 
 
 if __name__ == "__main__":
@@ -94,6 +115,7 @@ if __name__ == "__main__":
         print("Expected A or B as unique argument!")
         exit(1)
 
+    # Citation plots
     cm = plt.get_cmap('gist_rainbow')
     fig = plt.figure()
     ax = fig.add_subplot(111)
@@ -127,5 +149,31 @@ if __name__ == "__main__":
     ax.boxplot(aggs, notch=True)
     ax.set_xticklabels([dset for dset, _ in dsets.items()],
                        rotation=30)
+    plt.show()
 
+    # Hindex plots
+    cm = plt.get_cmap('gist_rainbow')
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_prop_cycle('color', rainbow)
+    plt.title(f"(Max) h-index of papers for years={years}")
+    plt.xlabel("Top percentiles")
+    plt.ylabel("Papers with h-index >= than quantile")
+    aggs = []
+    for i, (dset, fname) in enumerate(dsets.items()):
+        print(f"Going into conference {fname}")
+        aggs.append(load_bibs(fname, dset, i, hidx=True))
+        print(aggs[-1])
+    # Finalize plot
+    plt.xticks(np.array(range(len(qpcts))) + ((len(dsets) - 1) * width) / 2,
+               [str(p * 100) for p in qpcts])
+    plt.legend(loc="best")
+    plt.show()
+    # Boxplot graph now
+    fig, ax = plt.subplots()
+    plt.title(f"(Max) h-index for years={years}")
+    plt.ylabel("(max) h-index per paper")
+    ax.boxplot(aggs, notch=True)
+    ax.set_xticklabels([dset for dset, _ in dsets.items()],
+                       rotation=30)
     plt.show()
